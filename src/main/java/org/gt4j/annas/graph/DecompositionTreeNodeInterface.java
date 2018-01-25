f0d0366ffeb7f540edd20dc7722614ea8f8a3a6d
Philip-W <philipgwoods90@gmail.com>
2018-01-25 10:39:57 +0000

    Add method names for decomposition, no implementation code yet

--- src/main/java/org/gt4j/annas/graph/DecompositionTreeNodeInterface.java
+++ src/main/java/org/gt4j/annas/graph/DecompositionTreeNodeInterface.java
@@ -1,4 +1,5 @@
 package org.gt4j.annas.graph;
 
 public interface DecompositionTreeNodeInterface {
+
 }
--- src/main/java/org/gt4j/annas/graph/util/DecomposeByCliqueCutset.java
+++ src/main/java/org/gt4j/annas/graph/util/DecomposeByCliqueCutset.java
@@ -1,8 +1,11 @@
 package org.gt4j.annas.graph.util;
 
 
+import org.gt4j.annas.graph.DecompositionTreeNodeInterface;
 import org.gt4j.annas.graph.EdgeInterface;
 import org.gt4j.annas.graph.GraphInterface;
+
+import java.util.List;
 
 /**
  * An implementation of a decomposition algorithm through the use of
@@ -21,12 +24,49 @@ import org.gt4j.annas.graph.GraphInterface;
 public class DecomposeByCliqueCutset<V, E extends EdgeInterface<V>> {
 
     GraphInterface<V, E> graph;
-
+    DecompositionTreeNodeInterface treeRoot;
 
     public DecomposeByCliqueCutset(final GraphInterface<V, E> inputGraph){
+        this.graph = inputGraph;
+    }
 
+    private List<V> getMinimalOrdering(){
+        return null;
+    }
+
+    private GraphInterface<V, E> getFillInSet(){
+        return null;
+    }
+
+    /**
+     *
+     * @return DecompositionTreeNodeInterface
+     *          This method will return the root of the tree after construction
+     */
+    private DecompositionTreeNodeInterface runDecomposition(){
+        //Get Ordering
+        //get fill in
+        // Get set c(v)
+        //call recursive decomposition?
+        return treeRoot;
+    }
+
+    /**
+     *
+     * @param treeNode
+     *          A graph
+     * @return DecompositionTreeNodeInterface
+     *          Recursive calls will be made each returning a new layer of the
+     *          tree, ultimately returning the root.
+     */
+    private DecompositionTreeNodeInterface decompose(GraphInterface<V, E> treeNode){
+
+        return null;
     }
 
 
-
+    public DecompositionTreeNodeInterface getDecomposition() {
+        treeRoot = runDecomposition();
+        return treeRoot;
+    }
 }
