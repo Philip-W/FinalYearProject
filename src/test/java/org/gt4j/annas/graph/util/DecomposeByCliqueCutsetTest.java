5993b7b768f3149b5a826a97e60dddef3db51dbb
Philip-W <philipgwoods90@gmail.com>
2018-01-25 15:04:13 +0000

    Add test class for decomposition

---
+++ src/test/java/org/gt4j/annas/graph/util/DecomposeByCliqueCutsetTest.java
@@ -0,0 +1,12 @@
+package org.gt4j.annas.graph.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DecomposeByCliqueCutsetTest {
+    @Test
+    public void getDecomposition() throws Exception {
+    }
+
+}
