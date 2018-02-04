e5d8f3cc8575078cc99e9043efaf9f2092d20a8f
Philip-W <philipgwoods90@gmail.com>
2018-02-04 15:28:08 +0000

    Added framework methods for B_u Testing

--- src/main/java/org/gt4j/annas/graph/classifier/IsGu.java
+++ src/main/java/org/gt4j/annas/graph/classifier/IsGu.java
@@ -3,6 +3,7 @@ package org.gt4j.annas.graph.classifier;
 import org.gt4j.annas.graph.DecompositionTreeNodeInterface;
 import org.gt4j.annas.graph.EdgeInterface;
 import org.gt4j.annas.graph.GraphInterface;
+import org.gt4j.annas.graph.SimpleUndirectedGraph;
 
 public class IsGu<V, E extends EdgeInterface<V>> implements Classifier<V, E>  {
 
@@ -14,6 +15,30 @@ public class IsGu<V, E extends EdgeInterface<V>> implements Classifier<V, E>  {
 
     public boolean classify(DecompositionTreeNodeInterface root){
 
+        return false;
+    }
+
+    /**
+     * Tests the following condition:
+     *      * G has exactly one nontrivial anticomponent, and this anticomponent
+     *          is a long hole (Cycle length >=5)
+     *
+     * @param graph
+     * @return
+     */
+    private boolean isLongHole(SimpleUndirectedGraph<V, E> graph){
+        return false;
+    }
+
+    /**
+     * Tests the following condition:
+     *  * All non-trivial anticomponents of G are isomorphic to the complement of
+     *      K_2.
+     *
+     * @param graph
+     * @return
+     */
+    private boolean isIsomorphic(SimpleUndirectedGraph<V, E> graph){
         return false;
     }
 }
