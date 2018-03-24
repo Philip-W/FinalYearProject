03f950ae160f0ad38844a3aef3a8c1370a6e85c1
Philip-W <philipgwoods90@gmail.com>
2018-03-24 16:55:48 +0000

    Added max clique arrays for decomp leaf

--- src/main/java/org/gt4j/annas/graph/DecompositionTreeLeaf.java
+++ src/main/java/org/gt4j/annas/graph/DecompositionTreeLeaf.java
@@ -2,14 +2,18 @@ package org.gt4j.annas.graph;
 
 import org.gt4j.annas.graph.classifier.IsGu;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class DecompositionTreeLeaf <V, E extends EdgeInterface<V>> implements DecompositionTreeNodeInterface {
 
 
     GraphInterface<V, E> leafGraph;
-    private boolean isBu;
+    // Default b_u to false as it is unclassified
+    private boolean isBu = false;
     private IsGu.Type BuType;
+
+    private ArrayList<V> maxWeightClique;
 
     /* For decomp leaves the cutset is the set used to seperate the leaf from
     the previous inner node
@@ -41,7 +45,15 @@ public class DecompositionTreeLeaf <V, E extends EdgeInterface<V>> implements De
     public void setBuType(IsGu.Type type){ BuType = type; }
 
     public boolean getBu(){return isBu;}
+
     public IsGu.Type getBuType() { return BuType; }
 
+    public void setMaxWeightClique(ArrayList<V> maxWeightClique) {
+        this.maxWeightClique = maxWeightClique;
+    }
+
+    public ArrayList<V> getMaxWeightCliqe(){
+        return maxWeightClique;
+    }
 }
 
