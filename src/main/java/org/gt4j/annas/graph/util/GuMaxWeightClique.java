f177d4d0e51201a40857b964c63efab8d79c67d6
Philip-W <philipgwoods90@gmail.com>
2018-03-24 17:33:03 +0000

    Added traversal for maxClique

--- src/main/java/org/gt4j/annas/graph/util/GuMaxWeightClique.java
+++ src/main/java/org/gt4j/annas/graph/util/GuMaxWeightClique.java
@@ -1,17 +1,57 @@
 package org.gt4j.annas.graph.util;
 
-import org.gt4j.annas.graph.DecompositionTreeNodeInterface;
-import org.gt4j.annas.graph.EdgeInterface;
-import org.gt4j.annas.graph.WeightedVertex;
+import org.gt4j.annas.graph.*;
+import org.gt4j.annas.graph.classifier.IsGu;
 
 import java.util.ArrayList;
 
 public class GuMaxWeightClique<V extends WeightedVertex,
         E extends EdgeInterface<V>> {
 
+    /**
+     * Sets the max weight clique of the given leaf (storing it in the leaf)
+     * and returns the sum of the weighted clique
+     * @param leaf
+     * @return
+     */
+    public ArrayList<V> maxCliqueInLeaf(DecompositionTreeLeaf<V, E> leaf){
+            if (!leaf.getBu()) return null ; // Should throw unClassified graph exception
+            if (leaf.getBuType() == IsGu.Type.K2){
+                maxCliqueInK2();
+            }
+
+        return null;
+    }
+
+    public ArrayList<V> maxCliqueInK2(DecompositionTreeLeaf<V, E> leaf){
+
+
+    }
 
     public ArrayList<V> getMaxClique(DecompositionTreeNodeInterface<V, E> root){
+        ArrayList<V> maxClique = null;
+        if (root.isLeaf()){
+            maxClique = maxCliqueInLeaf((DecompositionTreeLeaf<V, E>) root);
+        }
+        DecompositionTreeInnerNode<V, E> temp =
+                (DecompositionTreeInnerNode<V, E>) root;
 
-        return new ArrayList<>();
+        ArrayList<DecompositionTreeLeaf> leaves = new ArrayList<>();
+        leaves.addAll(temp.getLeaves());
+
+        while (temp.getInnerChildren().size() != 0){
+            temp = temp.getInnerChildren().get(0);
+            leaves.addAll(temp.getLeaves());
+        }
+
+
+        for (DecompositionTreeLeaf leaf : leaves){
+            maxCliqueInLeaf(leaf);
+        }
+
+        return maxClique;
     }
+
+
+    ArrayList<>
 }
