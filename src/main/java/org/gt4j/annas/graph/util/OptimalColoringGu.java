b49497e559c661f991fda0d69d52f0f54fbf16fa
Philip-W <philipgwoods90@gmail.com>
2018-03-25 17:05:53 +0100

    Added leave traversal to color each leave individual

--- src/main/java/org/gt4j/annas/graph/util/OptimalColoringGu.java
+++ src/main/java/org/gt4j/annas/graph/util/OptimalColoringGu.java
@@ -1,8 +1,11 @@
 package org.gt4j.annas.graph.util;
 
+import org.gt4j.annas.graph.DecompositionTreeInnerNode;
+import org.gt4j.annas.graph.DecompositionTreeLeaf;
 import org.gt4j.annas.graph.DecompositionTreeNodeInterface;
 import org.gt4j.annas.graph.EdgeInterface;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class OptimalColoringGu<V,  E extends EdgeInterface<V>>
@@ -10,17 +13,47 @@ public class OptimalColoringGu<V,  E extends EdgeInterface<V>>
 
 
     DecompositionTreeNodeInterface<V, E> root;
+    OptimalColoringBasicGu<V, E> colorBasic;
 
     public OptimalColoringGu(DecompositionTreeNodeInterface<V, E> node){
         root = node;
+        colorBasic = new OptimalColoringBasicGu<>();
     }
 
 
+    private void colorLeaves(){
+        if (root.isLeaf()){
+            colorBasic.computeColoring((DecompositionTreeLeaf<V, E>) root);
+            return;
+        }
+        DecompositionTreeInnerNode<V, E> temp =
+                (DecompositionTreeInnerNode<V, E>) root;
+
+        ArrayList<DecompositionTreeLeaf> leaves = new ArrayList<>();
+        leaves.addAll(temp.getLeaves());
+
+        while (temp.getInnerChildren().size() != 0){
+            temp = temp.getInnerChildren().get(0);
+            leaves.addAll(temp.getLeaves());
+        }
+        
+        // Establish a coloring of each leaf
+        for (DecompositionTreeLeaf leaf : leaves){
+            colorBasic.computeColoring(leaf);
+        }
+
+    }
+
+    private void permuteLeafColors() {
+
+    }
 
     public void setOptimalColoring(){
         // Traverse tree, get coloring for each leaf
         // recursively permute the coloring from the bottom up.
 
+        colorLeaves();
+        permuteLeafColors();
     }
 
 
