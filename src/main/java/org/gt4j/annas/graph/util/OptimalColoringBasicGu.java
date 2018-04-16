c4bf7922b9b0ca67a14916f3f55df3d5fd4cea1b
Philip-W <philipgwoods90@gmail.com>
2018-04-16 18:19:04 +0100

    Successfully coloured basic graph: holes

--- src/main/java/org/gt4j/annas/graph/util/OptimalColoringBasicGu.java
+++ src/main/java/org/gt4j/annas/graph/util/OptimalColoringBasicGu.java
@@ -2,6 +2,7 @@ package org.gt4j.annas.graph.util;
 
 import org.gt4j.annas.graph.DecompositionTreeLeaf;
 import org.gt4j.annas.graph.EdgeInterface;
+import org.gt4j.annas.graph.SimpleUndirectedGraph;
 import org.gt4j.annas.graph.classifier.IsGu;
 
 import java.util.Collection;
@@ -10,7 +11,8 @@ public class OptimalColoringBasicGu<V,  E extends EdgeInterface<V>> {
 
 
     private int colorLongHole(DecompositionTreeLeaf<V, E> leaf){
-        int color = 1;
+        int color = 1; // This is the next free colour
+
         for (Collection<V> component : leaf.getAntiComponents()){
             // Vertex is complete to every other vertex, should be unique color
             if (component.size() == 1){
@@ -21,14 +23,21 @@ public class OptimalColoringBasicGu<V,  E extends EdgeInterface<V>> {
             else {
                 V vertex = (V) component.toArray()[0];
                 leaf.setVertexColor(vertex, color);
-                leaf.getLeafGraph().getEdges(vertex);
+                //leaf.getLeafGraph().getEdges(vertex);
                 V adj1, adj2; // 2 adjacent neighbours
-                vertex = (V) leaf.getLeafGraph().getEdges(vertex).toArray()[0];
+                SimpleUndirectedGraph<V, E> graph =
+                        InducedSubgraph.inducedSubgraphOf(
+                                (SimpleUndirectedGraph<V, E>) leaf.getLeafGraph()
+                                , component);
+
+                E edge = (E) graph.getEdges(vertex).toArray()[0];
+                vertex = edge.getOtherEndpoint(vertex);
+
 
                 while (true){
                     // This should only return 2 edges! requires testing.
-                    adj1 = (V) leaf.getLeafGraph().getEdges(vertex).toArray()[0];
-                    adj2 = (V) leaf.getLeafGraph().getEdges(vertex).toArray()[1];
+                    adj1 =  ((E)graph.getEdges(vertex).toArray()[0]).getOtherEndpoint(vertex);
+                    adj2 = ((E)graph.getEdges(vertex).toArray()[1]).getOtherEndpoint(vertex);;
                     int leftColor = leaf.getVertexColor(adj1);
                     int rightColor = leaf.getVertexColor(adj2);
 
@@ -67,8 +76,9 @@ public class OptimalColoringBasicGu<V,  E extends EdgeInterface<V>> {
                 int colorsused = component.size() % 2;
                 color += colorsused == 1 ? 3 : 2;
             }
+
         }
-        return color;
+        return --color;
     }
 
 
